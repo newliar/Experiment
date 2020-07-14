5c8c5cb56d45583ed27c7fdf9a60188e6d970f5c
newliar <1107325513@qq.com>
2020-07-14 21:08:40 +0800

    json file

--- test/src/read_map_json.py
+++ test/src/read_map_json.py
@@ -1,7 +1,10 @@
 import json
 import os
+import numpy as np
 
-def getroad():
+
+
+def get_road():
     file_path = os.path.dirname(os.getcwd())+"\\dataset\\allways.json"
     file = open(file_path, 'r', encoding='utf-8')
     # 存放 干线公路、一级道路、二级道路、三级道路
@@ -14,10 +17,6 @@ def getroad():
     primary_link = []
     secondary_link = []
     tertiary_link = []
-    # 道路的ref
-    trunk_ref = []
-    trunk_link_ref = []
-
     #  将道路存放到对应的list
     for line in file.readlines():
         dic = json.loads(line)
@@ -64,12 +63,12 @@ def getroad():
                 secondary_link.append(dic)
             elif dic["tag"]["v"] == "tertiary_link":
                 tertiary_link.append(dic)
-
-
-    print(get_way_ref(trunk_link, trunk_link_ref))
+    return(trunk, primary, secondary, tertiary, trunk_link, primary_link, secondary_link, tertiary_link)
 
 # 获得道路的所有ref值
-def get_way_ref(way, way_ref):
+def get_way_ref(way):
+    # 存储way的ref值
+    way_ref = []
     for ele in way:
         temp = []
         for nd_ele in ele["nd"]:
@@ -78,5 +77,34 @@ def get_way_ref(way, way_ref):
         way_ref.append(temp)
     return way_ref
 
+# 获得所有node之间的连接情况
+def get_node_relation(way_list):
+    # 获得ref的总数量
+    count = 0
+    count_1 = 0
+    # list类型
+    node_relation = []
+    for m in way_list:
+        for n in m:
+            if n not in node_relation:
+                node_relation.append(n)
+                count += 1
+            count_1 += 1
+    print(count)
+    print(count_1)
+
+    # 存储各个ref的连通状态
+    node_array = np.zeros([count, count])
+    # i j是node_array下标
+    i = 0
+    for m in way_list:
+        j = 0
+        for n in m:
+            index = node_relation.index(n)
+            node_array[index][index] = 1
+    print(node_array)
+
+
 if __name__ == "__main__":
-    getroad()
+    way_tuple = get_road()
+    get_node_relation(get_way_ref(way_tuple[0]))
