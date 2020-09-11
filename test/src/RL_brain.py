9c0e931ab021c9bbf90d16359332cdf8624aba89
newliar <1107325513@qq.com>
2020-09-11 14:40:59 +0800

    add RL module

--- test/src/RL_brain.py
+++ test/src/RL_brain.py
@@ -4,7 +4,7 @@ from cross_env import Cross
 
 
 class QLearningTable:
-    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
+    def __init__(self, actions, learning_rate=1, reward_decay=0.9, e_greedy=0.9):
         self.actions = actions
         self.lr = learning_rate
         self.gamma = reward_decay
@@ -12,26 +12,34 @@ class QLearningTable:
         self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float64)
 
     def choose_action(self, observation, env):
-        self.check_state_exist(observation, env)
+        self.check_state_exist(observation)
         length = env.get_length(observation)
+        index_flag = False
         if np.random.uniform() < self.epsilon:
-            state_action = self.q_table.loc[observation, self.actions[0:length]]
+            state_action = self.q_table.loc[observation, :]
             action = np.random.choice(state_action[state_action == np.max(state_action)].index)
-            index = self.actions[action].index
+            index = self.actions.index(action)
+            if index > length - 1:
+                index_flag = True
         else:
-            action = np.random.choice(self.actions[0:length])
-            index = self.actions[action].index
-        return index
+            action = np.random.choice(self.actions)
+            index = self.actions.index(action)
+            if index > length - 1:
+                index_flag = True
+        return index, index_flag
 
-    def learn(self, s, i, r, s_, i_):
-        # TODO:不能之间加减 Q 表，必须取到对应元素 e.g. 新 Q(s1,a2) = 老 Q(s1,a2) + α * 差距      差距=现实-估计
+    def learn(self, s, i, r, s_, env):
         self.check_state_exist(s_)
         q_predict = self.q_table.loc[s, self.actions[i]]
         if s_ != 'terminal':
-            q_target = r + self.gamma * self.q_table.loc[s_, self.actions[i_]].max()
+            q_target = r + self.gamma * self.q_table.loc[s_, :].max()
         else:
             q_target = r
         self.q_table.loc[s, self.actions[i]] += self.lr * (q_target - q_predict)
+        # print('--------------------------------------------------------')
+        # print(self.q_table)
+
+        return self.q_table
 
     def check_state_exist(self, state):
         if state not in self.q_table.index:
--- test/src/cross_env.py
+++ test/src/cross_env.py
@@ -1,13 +1,14 @@
-import numpy as np
+import tools
 
 
 class Cross:
-    def __init__(self, next_state_list, action_list, distance_list, start_point, end_point, n_states=559):
+    def __init__(self, next_state_list, action_list, distance_list, start_point, end_point, cross_info, n_states=559):
         self.next_state_list = next_state_list
         self.action_list = action_list
         self.distance_list = distance_list
         self.start_point = start_point
         self.end_point = end_point
+        self.cross_info = cross_info
         self.n_states = n_states
 
     def get_next_states(self, state):
@@ -22,13 +23,26 @@ class Cross:
     def get_length(self, state):
         return len(self.action_list[state])
 
-    def step(self, state, index):
-        s_ = self.get_next_state[state][index]
-        if index == self.end_point:
-            reward = 1
+    def step(self, state, index, index_flag):
+        if index_flag:
+            reward = -1
             done = True
             s_ = 'terminal'
         else:
-            reward = 1 / self.get_distance[state][index]
-            done = False
+            s_ = self.get_next_state(state, index)
+            angle_1 = tools.getDegree(self.cross_info[state][5], self.cross_info[state][6],
+                                      self.cross_info[self.end_point][5], self.cross_info[self.end_point][6])
+            angle_2 = tools.getDegree(self.cross_info[state][5], self.cross_info[state][6],
+                                      self.cross_info[self.next_state_list[state][index]][5], self.cross_info[self.next_state_list[state][index]][6])
+            if s_ == self.end_point:
+                reward = 1
+                done = True
+                s_ = 'terminal'
+                print('get it')
+            elif 100 < abs(angle_1 - angle_2) < 260:
+                reward = -(1 / self.get_distance(state, index))
+                done = False
+            else:
+                reward = 1 / self.get_distance(state, index)
+                done = False
         return s_, reward, done
